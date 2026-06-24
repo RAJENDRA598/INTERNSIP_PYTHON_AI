1664684e5d00fe0400847a986f2a20c9f1ab6fea
RAJENDRA598 <Tammineedirajendra091@gmail.com>
2026-06-24 21:13:55 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,jsonify
+'''from flask import Flask, request,jsonify
 import psycopg2
 
 app =Flask(__name__)
@@ -106,6 +106,71 @@ def delete_data():
     connection.close()
     return jsonify({"message":"Data deleted successfully"}),200
 
+
+if __name__ == "__main__":
+    app.run(debug=True)'''
+    
+    
+from flask import Flask, request,jsonify
+import psycopg2
+from flask_bcrypt import Bcrypt
+
+app =Flask(__name__)
+
+bcrypt = Bcrypt(app)
+
+#database config
+DB_HOST = "localhost"
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASSWORD = "0598"
+
+def get_db_connection():
+    return psycopg2.connect(
+        host = DB_HOST,
+        database = DB_NAME,
+        user =DB_USER,
+        password =DB_PASSWORD
+    )
+
+#CREATE STUDENT_TABLE
+def create_student_table():
+    connection = get_db_connection()
+    cur = connection.cursor()
+    cur.execute("""
+             CREATE TABLE IF NOT EXISTS users_table(
+                 user_id SERIAL PRIMARY KEY,
+                 username TEXT NOT NULL,
+                 password TEXT NOT NULL,
+                 email TEXT NOT  NULL UNIQUE
+                );
+""")
+    connection.commit()
+    cur.close()
+    connection.close()
+
+create_student_table()
+
+@app.route('/signup', methods = ['POST'])
+def signup():
+
+    username =request.json["username"]
+    email =request.json["email"]
+    password =request.json["password"]
+    
+    hashed_username= bcrypt.generate_password_hash(username).decode("utf-8")
+    '''hashed_password= bcrypt.generate_password_hash(password).decode('utf-8')
+    hashed_email= bcrypt.generate_password_hash(email).decode('utf-8')'''
+
+    connection = get_db_connection()
+    cur = connection.cursor()
+    cur.execute("""
+         INSERT INTO users_table(username,email,password) VALUES(%s,%s,%s)
+""",(hashed_username,email,password))#use hashed_username,hashed_password,hashed_email
+    connection.commit()
+    cur.close()
+    connection.close()
+    return jsonify({"message":"signup successful"})
 
 if __name__ == "__main__":
     app.run(debug=True)
